8536c4c1bbdafea5f044cabd9aa62f8ed4064551
MohanRajK2993 <mohanra.kamalanathan@servicenow.com>
2026-08-31 16:13:37 +0530

    Add config template and update gitignore1

--- tests/api/GetIdentityTest.java
+++ tests/api/GetIdentityTest.java
@@ -24,6 +24,7 @@ public class GetIdentityTest {
 	            .auth().basic(username, password)
 	            .header("Content-Type", "application/json")
 	        
+	        
 	        .when()
 	            .get(fullURL);
 	    
